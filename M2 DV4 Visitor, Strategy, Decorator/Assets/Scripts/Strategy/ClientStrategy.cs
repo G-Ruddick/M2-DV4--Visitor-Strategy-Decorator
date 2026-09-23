e45e702681d3afc041a4b14e87a1716725911003
Orangesuit1 <glen@ruddicks.net>
2026-09-22 22:12:15 -0700

    chapter 11

---
+++ M2 DV4 Visitor, Strategy, Decorator/Assets/Scripts/Strategy/ClientStrategy.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Chapter.Strategy {
+    public class ClientStrategy : MonoBehaviour {
+        private GameObject _drone;
+        private List<IManeuverBehaviour> _components = new List<IManeuverBehaviour>();
+        
+        private void SpawnDrone() {
+            _drone = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            _drone.AddComponent<Drone>();
+            _drone.transform.position = Random.insideUnitSphere * 10;
+
+            ApplyRandomStrategies();
+        }
+
+        private void ApplyRandomStrategies() {
+            _components.Add(_drone.AddComponent<WeavingManeuver>());
+            _components.Add(_drone.AddComponent<BoppingManeuver>());
+            _components.Add(_drone.AddComponent<FallbackManeuver>());
+
+            int index = Random.Range(0, _components.Count);
+            _drone.GetComponent<Drone>().ApplyStrategy(_components[index]);
+        }
+        
+        void OnGUI() {
+            if (GUI.Button(new Rect(0, 80, 120, 30), "Spawn Drone")) {
+                SpawnDrone();
+            }
+        }
+    }
+}
---
+++ M2 DV4 Visitor, Strategy, Decorator/Assets/Scripts/Strategy/IManeuverBehavior.cs
@@ -0,0 +1,5 @@
+namespace Chapter.Strategy {
+    public interface IManeuverBehaviour {
+        void Maneuver(Drone drone);
+    }
+}
